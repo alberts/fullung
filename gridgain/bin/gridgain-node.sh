7fdbc7f966022b308e4f897b84c30a895d46c2cb
fullung <fullung@76970873-ce3a-0410-a9b7-c9c6e07dc4f1>
2007-09-27 00:07:00 +0000

    GridGain goodies.

--- gridgain/bin/gridgain-node.sh
+++ gridgain/bin/gridgain-node.sh
@@ -1,3 +1,31 @@
 #!/bin/sh
 
-nice -n 19 ./wrapper-linux-x86-32 -c ../config/wrapper-node.conf
+if [ -z "$GRIDGAIN_HOME" ] ; then
+  ## resolve links - $0 may be a link to activemq's home
+  PRG="$0"
+  progname=`basename "$0"`
+  saveddir=`pwd`
+
+  # need this for relative symlinks
+  dirname_prg=`dirname "$PRG"`
+  cd "$dirname_prg"
+
+  while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '.*/.*' > /dev/null; then
+    PRG="$link"
+    else
+    PRG=`dirname "$PRG"`"/$link"
+    fi
+  done
+
+  GRIDGAIN_HOME=`dirname "$PRG"`/..
+
+  cd "$saveddir"
+
+  # make it fully qualified
+  GRIDGAIN_HOME=`cd "$GRIDGAIN_HOME" && pwd`
+fi
+
+$GRIDGAIN_HOME/bin/wrapper-linux-x86-32 -c $GRIDGAIN_HOME/config/wrapper-node.conf $@
