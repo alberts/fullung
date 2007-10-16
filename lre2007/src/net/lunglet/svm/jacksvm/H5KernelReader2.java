1711df18046efc37bf6a73d5c34576feebf11d6e
fullung <fullung@76970873-ce3a-0410-a9b7-c9c6e07dc4f1>
2007-10-16 12:03:01 +0000

    Optimization: use array instead of Map for LUT.

--- lre2007/src/net/lunglet/svm/jacksvm/H5KernelReader2.java
+++ lre2007/src/net/lunglet/svm/jacksvm/H5KernelReader2.java
@@ -3,9 +3,9 @@ package net.lunglet.svm.jacksvm;
 import com.googlecode.array4j.Storage;
 import com.googlecode.array4j.io.HDFReader;
 import com.googlecode.array4j.packed.FloatPackedMatrix;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import net.lunglet.hdf.DataSet;
+import net.lunglet.hdf.DataSpace;
 import net.lunglet.hdf.H5File;
 
 /**
@@ -16,23 +16,31 @@ import net.lunglet.hdf.H5File;
 public final class H5KernelReader2 implements KernelReader {
     private final FloatPackedMatrix kernel;
 
-    private final Map<Integer, Integer> orderLut;
+    private final int[] orderLut;
 
     public H5KernelReader2(final H5File kernelh5) {
-        DataSet kernelds = kernelh5.getRootGroup().openDataSet("/kernel");
-        int[] order = kernelds.getIntArrayAttribute("order");
-        this.orderLut = new HashMap<Integer, Integer>();
+        DataSet orderds = kernelh5.getRootGroup().openDataSet("/order");
+        DataSpace space = orderds.getSpace();
+        int[] order = new int[(int) space.getDim(0)];
+        space.close();
+        orderds.read(order);
+        orderds.close();
+        int maxOrder = -1;
+        for (int i : order) {
+            maxOrder = i > maxOrder ? i : maxOrder;
+        }
+        this.orderLut = new int[maxOrder + 1];
+        Arrays.fill(orderLut, -1);
         for (int i = 0; i < order.length; i++) {
-            orderLut.put(order[i], i);
+            orderLut[order[i]] = i;
         }
         // use heap storage to avoid large direct memory allocations
         this.kernel = FloatPackedMatrix.createSymmetric(order.length, Storage.HEAP);
         new HDFReader(kernelh5).read("/kernel", kernel);
-        kernelds.close();
     }
 
     public float read(final int i, final int j) {
-        return kernel.get(orderLut.get(i), orderLut.get(j));
+        return kernel.get(orderLut[i], orderLut[j]);
     }
 
     public FloatPackedMatrix read(final int[] indexes) {
