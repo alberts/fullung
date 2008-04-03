f0565f820c7967f86864d64aec27c82ff4a852e7
fullung <fullung@76970873-ce3a-0410-a9b7-c9c6e07dc4f1>
2008-04-03 21:48:39 +0000

    Generate XML using only model and test seg keys.

--- sre2008/scripts/evals/2006/eval2006.py
+++ sre2008/scripts/evals/2006/eval2006.py
@@ -16,8 +16,11 @@ def read_train(filename, models, gender, condition):
         models[id]['train'] = set()
         train = train.split(',')
         for trn in train:
-            # XXX this doesn't work with 2w
-            sph, channel = trn.split(':', 2)
+            parts = trn.split(':')
+            if len(parts) == 1:
+                sph, channel = parts[0], 'a'
+            else:
+                sph, channel = parts
             sph = sph.split('.')[0]
             channel = channel.lower()
             assert channel in ('a', 'b')
@@ -30,124 +33,35 @@ def read_train(filename, models, gender, condition):
         models[id]['trials'] = {}
     return models
 
-def remove_model_errors(filename, models):
-    lines = open(filename).readlines()
-    for line in lines:
-        line = line.strip()
-        if line.find('#') == 0: continue
-        id = line.strip().lower()
-        if not models.has_key(id): continue
-        print >>sys.stderr, 'removing bad model %s' % id
-        del models[id]
-
-def read_trials(filename, models, traincond, testcond):
-    lines = open(filename).readlines()
-    for line in lines:
-        line = line.strip()
-        id, gender, testseg, channel = re.split('\\s+', line)
-        id = id.strip().lower()
-        assert models.has_key(id)
-        assert models[id]['condition'] == traincond
-        gender = gender.strip().lower()
-        assert gender in ('m', 'f')
-        testseg = testseg.strip().lower()
-        channel = channel.strip().lower()
-        assert channel in ('a', 'b')
-        assert gender == models[id]['gender']
-        testkey = testseg, channel
-        assert testkey not in models[id]['trials']
-        trial = {'type' : 'unk', 'condition' : testcond, 'gender' : gender}
-        models[id]['trials'][testkey] = trial
-
-def remove_trial_errors(filename, models):
-    lines = open(filename).readlines()
-    for line in lines:
-        line = line.strip()
-        if line.find('#') == 0: continue
-        testseg, testsegmod = re.split('\\s+', line, 1)
-        testsegmod = re.split('\\s+', testsegmod)
-        badids = []
-        for id in testsegmod:
-            # remove for all models
-            if id.strip().lower() == 'm0000':
-                badids = models.keys()
-                break
-            else:
-                badids.append(id.strip().lower())
-        for id in badids:
-            if not models.has_key(id): continue
-            for channel in ('a', 'b'):
-                testkey = testseg, channel
-                if testkey not in models[id]['trials']: continue
-                print >> sys.stderr, 'removing bad trial %s channel %s for model %s' % (testkey + (id,))
-                del models[id]['trials'][testkey]
-
-def read_model_key(filename, models):
-    lines = open(filename).readlines()
-    for line in lines:
-        line = line.strip()
-        parts = re.split('\\s+', line)
-        id = parts[0]
-        speakerid = parts[2]
-        gender = parts[3]
-        if not models.has_key(id): continue
-        assert models[id]['gender'] == gender
-        models[id]['pin'] = speakerid
-
 def read_key(filename, models):
     lines = open(filename).readlines()
-    key = {}
-    for line in lines:
-        line = line.strip()
-        parts = re.split('\\s+', line)
-        seg = parts[0]
-        speakerid = parts[4]
+    lines = [re.split('\\s+', line.strip()) for line in lines]
+    for parts in lines:
+        state = parts[15]
+        assert state in ('target', 'nontarget', 'bad')
+        if state == 'bad': continue
+        if state == 'target':
+            state = 'targ'
+        elif state == 'nontarget':
+            state = 'non'
+        else: assert False
+        trialcond = parts[1].lower().strip()
+        id = parts[9].lower().strip()
+        model = models[id]
         gender = parts[5]
-        channel = parts[6].lower()
+        language = parts[7]
+        name = parts[0].lower().strip()
+        channel = parts[6].lower().strip()
         if channel not in ('a', 'b'): continue
-        segkey = seg, channel
-        if not key.has_key(segkey):
-            key[segkey] = speakerid
-        else:
-            assert speakerid == key[segkey]
-
-    badmodels = set()
-    for id in models:
-        modelspeaker = models[id]['pin']
-        trials = models[id]['trials']
-        badtrials = set()
-        for testkey in trials:
-            if not key.has_key(testkey):
-                badtrials.add(testkey)
-                continue
-            testspeaker = key[testkey]
-            trial = trials[testkey]
-            if testspeaker == modelspeaker:
-                trial['type'] = 'targ'
-            else:
-                trial['type'] = 'non'
-            trial['pin'] = testspeaker
-        for badtrial in badtrials:
-            condition = trials[badtrial]['condition']
-            print >>sys.stderr, 'removing unkeyed trial %s channel %s, condition %s' % \
-                (badtrial + (condition,))
-            del trials[badtrial]
-    for badmodel in badmodels:
-
-        print models[badmodel]['train']
-
-        condition = models[badmodel]['condition']
-        print >>sys.stderr, 'removing unkeyed model %s, condition %s' % \
-            (badmodel, condition)
-        del models[badmodel]
-
-def check_models(models):
-    for id in models:
-        train = models[id]['train']
-        trials = models[id]['trials']
-        badtrials = set()
-        for testkey, info in trials.iteritems():
-            assert info['type'] in ('targ', 'non')
+        trialkey = name, channel
+        pin = parts[4].lower().strip()
+        trial = {
+            'condition' : trialcond,
+            'gender' : gender,
+            'type' : state,
+            'pin' : pin
+            }
+        model['trials'][trialkey] = trial
 
 def print_eval(models):
     print '<?xml version="1.0" encoding="UTF-8"?>'
@@ -179,23 +93,27 @@ def print_eval(models):
         print '</model>'
     print '</eval>'
 
+def read_model_key(filename, models):
+    lines = open(filename).readlines()
+    for line in lines:
+        line = line.strip()
+        parts = re.split('\\s+', line)
+        id = parts[0]
+        speakerid = parts[2]
+        gender = parts[3]
+        if not models.has_key(id): continue
+        assert models[id]['gender'] == gender
+        models[id]['pin'] = speakerid
+
 def main():
     models = {}
-    conditions = ['10sec4w', '1conv4w', '3conv4w', '8conv4w']
-    for condition in conditions:
+    train_conditions = set(['10sec4w', '1conv4w', '3conv2w', '3conv4w', '8conv4w'])
+    for condition in train_conditions:
         filename = '%s.trn' % condition
         read_train(os.path.join('train', 'male', filename), models, 'm', condition)
         read_train(os.path.join('train', 'female', filename), models, 'f', condition)
-
-    for filename in glob(os.path.join('trials', '*.ndx')):
-        traincond, testcond = os.path.splitext(os.path.basename(filename))[0].split('-')
-        if traincond not in conditions: continue
-        read_trials(filename, models, traincond, testcond)
-    remove_model_errors('model-errors-v2-DAR.txt', models)
-    remove_trial_errors('testseg-errors-v5.txt', models)
-    read_model_key('sre06_model_key_v9.txt', models)
-    read_key('sre06_test_seg_key_v11.txt', models)
-    check_models(models)
+    read_model_key('sre06_model_key.txt', models)
+    read_key('sre06_test_seg_key_v11a.txt', models)
     print_eval(models)
 
 if __name__ == '__main__':
