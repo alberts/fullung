c115e0293edd062d212f8d5884288767e44bb9db
fullung <fullung@76970873-ce3a-0410-a9b7-c9c6e07dc4f1>
2008-09-22 13:01:04 +0000

    Documentation updates.

--- waves/read_grib_param.m
+++ waves/read_grib_param.m
@@ -10,6 +10,7 @@ end
 stime = {gribrec.stime};
 stime = stime(strcmp({gribrec.parameter},param));
 values = values.data;
+% set NaN values to 0 for interpolation to work
 values(isnan(values)) = 0;
 if size(values, 3) ~= length(stime)
     error('Number of values not equal to number of timestamps');
